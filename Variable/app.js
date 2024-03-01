30dbf46bd2957e906f14914eb861d8b4eb862320
abdullahnart <154362778+abdullahnart@users.noreply.github.com>
2024-03-01 23:21:14 +0500

    Add Chapter

---
+++ Variable/app.js
@@ -0,0 +1,59 @@
+// var nullVar = null;
+// console.log(nullVar);
+
+// const symbolVariable = Symbol ("Special Character to show in bracket");
+// console.log(symbolVariable);
+
+// The following all variables are legal ways to create a variable:
+
+// var Legal = "Legal";
+// var _Legal ="Legal";
+// var $Legal = "Legal";
+// var LegalVar = "Legal";
+// var LegalVar123 = "Legal";
+// var O21waisVar1 = "Legal";
+// var Legal_34_Var = "Legal";
+// var _Legal_Var = 2125;
+// var _Legal_Var_ = true;
+
+// The following all variables are illegal ways to create a variable:
+
+// var 1illLegal = "45"; 
+// var illLegal-Var = 12;
+// var illLegal Var = 317;
+// var / illegalVar = "Illaegal Var";
+// var /illegalVar = 45;
+
+
+// VARIABLES FOR STRINGS
+
+var username = "Abdullah";
+var myName = "Abdullah Naseem"
+alert(username);
+alert(myName);
+alert("Pizza\nPizz\nPiz\nPi\nP");
+var myEmail = "admin@gmail.com"
+alert("My Email is " +  myEmail);
+
+var jsBook = "A smarter way to learn JavaScript."
+alert("I am trying to learn from the book " + jsBook)
+
+// var showInWindow = "I can write HTML content through Javascript";
+// document.write(showInWindow);
+
+// var showInWindow = "I can write HTML content through Javascript";
+// console.log(showInWindow);
+
+var line = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(line);
+
+
+// VARIABLES FOR NUMBERS
+
+
+var myAge = 15;
+alert("I am " + myAge + " years old");
+
+var birthYear = 1998;
+document.write("My Birth year is " + birthYear + "<br>")
+document.write("Data type of My Declared Variable is " + typeof(birthYear));
