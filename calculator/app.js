2b88484d8a83da55574d2a35735441aac9179c10
abdullahnart <mabdullah.art2023@gmail.com>
2024-03-04 00:17:05 +0500

    Add Calculator Logics

---
+++ calculator/app.js
@@ -0,0 +1,14 @@
+document.write("This is my Calculator <br>")
+var num1 = +prompt("Enter a value");
+var num2 = +prompt("Enter a value");
+document.write("The Addition of two values ");
+document.write(num1 + num2);
+document.write();
+document.write("<br>The Subtraction of two values ");
+document.write(num1-num2);
+document.write("<br>The Mulipication of two values ");
+document.write(num1*num2);
+document.write("<br>The Division of two values ");
+document.write(num1/num2);
+document.write("<br>The Modulus of two values ");
+document.write(num1%num2);
