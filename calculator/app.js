8477d820f75719ec65b2a21a5480a6c3ef598dc8
abdullahnart <mabdullah.art2023@gmail.com>
2024-03-04 23:09:24 +0500

    Change logic of document.write

--- calculator/app.js
+++ calculator/app.js
@@ -1,14 +1,11 @@
+
+// If you declared variable once using var then you cannot declared that variable using let or const (It will give Error Syntax Error )
+// var a ="abc";
+// let a ="abc";
+// var a = "abcs";
+// document.write(a);
+
 document.write("This is my Calculator <br>")
 var num1 = +prompt("Enter a value");
 var num2 = +prompt("Enter a value");
-document.write("The Addition of two values is ");
-document.write(num1 + num2);
-document.write();
-document.write("<br>The Subtraction of two values is ");
-document.write(num1-num2);
-document.write("<br>The Mulipication of two values is ");
-document.write(num1*num2);
-document.write("<br>The Division of two values is ");
-document.write(num1/num2);
-document.write("<br>The Modulus of two values is ");
-document.write(num1%num2);
+document.write("The Addition of two values is", num1 + num2, "<br>The Subtraction of two values is ", num1-num2,"<br>The Mulipication of two values is ", num1*num2, "<br>The Division of two values is ", num1/num2, "<br>The Modulus of two values is ", num1%num2 );
