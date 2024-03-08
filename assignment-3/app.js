d6fa4d2e2aae955fdafd5e3d47a281f788ec4c54
abdullahnart <mabdullah.art2023@gmail.com>
2024-03-08 13:17:35 +0500

    Added all variable in document.write

--- assignment-3/app.js
+++ assignment-3/app.js
@@ -10,6 +10,9 @@ var user_f_name = "user_name";
 var $username = "$username";
 var username9 = "username9"
 
+document.write('<h2 style = "margin-bottom:0px;">Legal Variable</h2><br>var users = "Users"; <br> var userName = "User Name"; <br> var user_f_name = "user_name"; <br> var $username = "$username"; <br> var username9 = "username9"')
+
+
 // Illegal Variable
 
 // var 123username = "123username";
@@ -17,6 +20,9 @@ var username9 = "username9"
 // var user-name = "user-name";
 // var *username = "*username";
 // var function = "javascript reserved keyword";
+
+document.write('<h2 style = "margin-bottom:0px;">Illegal Variable</h2><br>var 123username = "123username"; <br> var user name= "user name"; <br> var user-name = "user-name"; <br> var *username = "*username"; <br> var function = "javascript reserved keyword";')
+
 
 document.write("<h2>Rules for naming JS variables</h2> <br> b) Variable names can only contain <b>letters</b>, <b>numbers</b>, <b>underscore</b> and <b>dollar</b>.<br> c) Variables must begin with a <b>aplphabet</b>, <b>dollar sign</b> or <b>underscore</b>. For example $name, _name or name <br>d) Variable names are case <b>sensitive</b><br>e) Variable names should not be JS <b>keyword</b>");
 
