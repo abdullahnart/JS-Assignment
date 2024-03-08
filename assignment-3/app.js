1dbe615e4770f5a95037aac10f2b7b8464f2d0d2
abdullahnart <mabdullah.art2023@gmail.com>
2024-03-08 13:10:18 +0500

    Complete Chapter 4

---
+++ assignment-3/app.js
@@ -0,0 +1,22 @@
+// 1. Declare 3 variables in one statement.
+var var1 = "value1", var2 = "value2", var3 = "value3";
+console.log(var1,var2,var3);
+
+// 2. Declare 5 legal & 5 illegal variable names.
+// Legal Variable
+var users = "Users";
+var userName = "User Name";
+var user_f_name = "user_name";
+var $username = "$username";
+var username9 = "username9"
+
+// Illegal Variable
+
+// var 123username = "123username";
+// var user name= "user name";
+// var user-name = "user-name";
+// var *username = "*username";
+// var function = "javascript reserved keyword";
+
+document.write("<h2>Rules for naming JS variables</h2> <br> b) Variable names can only contain <b>letters</b>, <b>numbers</b>, <b>underscore</b> and <b>dollar</b>.<br> c) Variables must begin with a <b>aplphabet</b>, <b>dollar sign</b> or <b>underscore</b>. For example $name, _name or name <br>d) Variable names are case <b>sensitive</b><br>e) Variable names should not be JS <b>keyword</b>");
+
