7cfcf866d1b8eec6155b422a565ae54f5c861b1c
abdullahnart <mabdullah.art2023@gmail.com>
2024-03-18 11:31:20 +0500

    Assignment 6

---
+++ assignment-6/app.js
@@ -0,0 +1,70 @@
+// let checkVar = prompt("Enter Character");
+// console.log(checkVar.toLowerCase() , checkVar.toUpperCase());
+
+
+// let inputVal1 = parseInt(prompt("Enter 1st Number"));
+// let inputVal2 = parseInt(prompt("Enter 2nd Number"));
+
+// if (inputVal1>inputVal2){
+//     console.log(inputVal1 + " is greater than " + inputVal2);
+// }
+// else if (inputVal1<inputVal2){
+//     console.log(inputVal1 + " is lesser than " + inputVal2);
+// }
+// else if (inputVal1===inputVal2){
+//     console.log(inputVal1 + " both are equals " + inputVal2);
+// }
+// else{
+//     console.log("Enter A Valid Number");
+// }
+
+
+// let inputNum = Number(prompt("Enter a Number"));
+
+// if(inputNum > 0){
+//     document.write("Your Number is positive " + inputNum);
+// }
+// else if (inputNum < 0) {
+//     document.write("Your Number is negative " + inputNum);
+// }else if (inputNum === 0 ){
+//     document.write("Your Number is " + inputNum);
+// }
+// else{
+//     document.write("Please Enter a Valid Number");
+// }
+
+
+// let vowels = prompt("Enter a Vowels").toLowerCase();
+
+// if(vowels === "a"){
+//     document.write(vowels + " is a vowel");
+// }
+// else if (vowels === "e") {
+//     document.write(vowels + " is a vowel");
+// }
+// else if (vowels === "i") {
+//     document.write(vowels + " is a vowel");
+// }
+// else if (vowels === "o") {
+//     document.write(vowels + " is a vowel");
+// }
+// else if (vowels === "u") {
+//     document.write(vowels + " is a vowel");
+// }
+// else{
+//     document.write("This is not vowels");
+// }
+
+// This is shortend Way to Find Vowels
+
+// let vowels = prompt("Enter a Vowels").toLowerCase();
+
+// if(vowels === "a" || vowels === "e" || vowels === "i" || vowels === "o" || vowels === "u") {
+//     document.write(vowels + " is a vowels");
+// }
+// else{
+//     document.write(vowels + " is not a vowels");
+// }
+
+
+
