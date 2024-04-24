1900dbe44e33f1d1765a0703bd9cb1922965658e
abdullahnart <mabdullah.art2023@gmail.com>
2024-04-25 00:32:50 +0500

    Assignment 7

---
+++ assignment-7/app.js
@@ -0,0 +1,23 @@
+
+// JS literal notation
+let studentNames = [];
+
+// JS object notation
+let student_names = new Array();
+
+// Declare and initialize a strings array.
+let arr = ["Sadiq","Owais","Kashif"]
+
+// Declare and initialize a numbers array.
+let arrNumber = [23,64,123,83,24,3]
+
+// Declare and initialize a boolean array.
+let arrBoolean = [true,false,false,true,true];
+
+// Declare and initialize a mixed array.
+let mixArray = ["Usama","Farhan",98,"Jamal",true,45,null,false,undefined];
+
+// Declare and Initialize an array and store available education qualifications in Pakistan
+let qualifications = ["SSC","HSC","BCS","BS","BCOM","MS","M. Phil.","PhD"];
+console.log(qualifications);
+
