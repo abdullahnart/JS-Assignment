7a5b4a5e9e93a2b331117f2d9f3098260050c5f6
abdullahnart <mabdullah.art2023@gmail.com>
2024-05-27 14:43:47 +0500

    Eid Mubarak Complete

--- eid-mubarak/app.js
+++ eid-mubarak/app.js
@@ -1,6 +1,6 @@
 function eidMubarak(){
     let wrapper = document.querySelector("#wrapper");
-    let eidDate = new Date();
+    let eidDate = new Date("June 17 2024");
     let currentDate = new Date ();
     let calculation = eidDate-currentDate;
     let second = calculation / 1000;
@@ -10,7 +10,7 @@ function eidMubarak(){
     if (day === 0 && hours === 0 && minutes === 0 && second === 0){
         wrapper.innerHTML = `<b>Eid<span>-ul-</span>Adha<span><br>Mubarak</span></b>`
     }else{
-        wrapper.innerHTML = `<h1>${Math.floor(day)} Days Remaining in Eid Edha 2024</h1>`
+        wrapper.innerHTML = `<b>${Math.floor(day)} Days Remaining in Eid Edha 2024</span></b>`
     }
 }
 eidMubarak();
