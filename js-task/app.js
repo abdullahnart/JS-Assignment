5facc4c8fac22caf9eb6e322eb6d673a2925d6d6
abdullahnart <mabdullah.art2023@gmail.com>
2024-08-12 23:36:50 +0500

    JS Task

---
+++ js-task/app.js
@@ -0,0 +1,14 @@
+let emptyArr = [];
+function returnArrayOfObject(arr1,arr2){
+    return arr1,arr2;
+}
+let array1 = [{
+    name:"Abudllah",
+    age:25
+}]
+let array2 = [{
+    name:"Sadiq",
+    age:26
+}]
+let finalArray = emptyArr.concat(array1,array2)
+console.log(finalArray);
